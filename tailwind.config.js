63745d41aa3c1d4bb4d95ba43d810912284cfdf7
Heloisa Abrantes Roncato <heloisa.dev@agroflux.ind.br>
2023-03-23 20:45:13 -0300

    color pallette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        faluRed: '#6f1d1b',
+        peach: '#efbc9b',
+        cambridgeBlue: '#70a288',
+        blackOlive: '#37423d',
+        outerSpace: '#435058',
+        brown: '#522b1f',
+        maroon: '#512c34',
+        lightMaroon: '#6a373b',
+        rose: '#93737e',
+        darkBrown: '#2e1209',
+        midnightGreen: '#073B4C',
+        columbiaBlue: '#B9D6F2',
+        viridianGreen: '#B9D6F2',
+        chocolateCosmos: '#4C061D',
+        melon: '#F9ADA0',
+        purplePink: '#9A7AA0',
+      },
+    },
   },
   plugins: [],
 }
